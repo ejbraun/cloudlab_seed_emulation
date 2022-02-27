b1f4a0eb1d05163380b56bd3d0c5f74389b302b2
evan <evanbraun@berkeley.edu>
2022-02-27 12:52:17 -0700

    initial files

---
+++ workerStartUp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get -qq --no-install-recommends install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/debian `lsb_release -cs` stable\"
+sudo apt-get update
+sudo apt-get -qq --no-install-recommends install docker-ce docker-ce-cli containerd.io docker-compose
+sudo docker swarm join --token $TOKEN $MANAGER_IP:2377
+sudo modprobe mpls_router
