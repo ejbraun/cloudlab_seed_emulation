037c27f1c2ae22d296b70b948028daffba66404a
evan <evanbraun@berkeley.edu>
2022-02-27 17:26:05 -0700

    let's see...

--- startEmulation.sh
+++ startEmulation.sh
@@ -1,10 +1,16 @@
-
-#1. extract swarm token from manager node
-#2. start up worker nodes (passing in token + ip address as env vars)
-#3. once number of nodes in swarm == number of nodes expected
-#3a. create docker registry
-    # docker service create --name registry --publish published=5000,target=5000 registry:2
-#3b. iterate through folders in priority.txt and do
-    # docker-compose -f ./containers/{folder_name}/docker-compose.yml build
-    # docker-compose -f ./containers/{folder_name}/docker-compose.yml push
-    # docker stack deploy --compose-file ./containers/{folder_name}/docker-compose.yml {folder_name}
+while [[ $(docker node ls | awk 'END{print NR}') != $1 ]];
+do
+    sleep 10
+    echo "waiting for all worker nodes to join swarm..."
+done
+# Create docker registry
+docker service create --name registry --publish published=5000,target=5000 registry:2
+# Start up services
+filename='priority.txt'
+while read p; do
+    docker-compose -f "containers/${p}/docker-compose.yml" build
+    docker-compose -f "containers/${p}/docker-compose.yml" push
+    docker stack deploy --compose-file "containers/$p/docker-compose.yml" "${p}"
+    echo "Deployed $p to swarm"
+    sleep 10
+done < "$filename"
--- workerStartUp.sh
+++ workerStartUp.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+MANAGER_IP=192.168.6.1
 sudo apt-get update
 sudo apt-get -qq --no-install-recommends install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/debian `lsb_release -cs` stable\"
 sudo apt-get update
 sudo apt-get -qq --no-install-recommends install docker-ce docker-ce-cli containerd.io docker-compose
+TOKEN=$(ssh -p 22 "root@$MANAGER_IP" "docker swarm join-token --quiet worker"))
 sudo docker swarm join --token $TOKEN $MANAGER_IP:2377
 sudo modprobe mpls_router
