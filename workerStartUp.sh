6ba84218dbd31a9f76236917c498917306605a43
evan <evanbraun@berkeley.edu>
2022-03-27 21:53:44 -0600

    omit interactive

--- workerStartUp.sh
+++ workerStartUp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MANAGER_IP=192.168.6.1
 sudo apt-get update
-sudo apt-get install \
+sudo apt-get -y install \
     ca-certificates \
     curl \
     gnupg \
@@ -11,7 +11,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
 TOKEN=$(ssh -o StrictHostKeyChecking=no -p 22 "root@node0" "docker swarm join-token --quiet worker")
 sudo docker swarm join --token $TOKEN $MANAGER_IP:2377
