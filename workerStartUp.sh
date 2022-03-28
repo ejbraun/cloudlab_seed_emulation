bf9ea84c294e7ef223084e14cb074cef106bb4cd
evan <evanbraun@berkeley.edu>
2022-03-27 22:07:38 -0600

    fix double update

--- workerStartUp.sh
+++ workerStartUp.sh
@@ -10,7 +10,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
 TOKEN=$(ssh -o StrictHostKeyChecking=no -p 22 "root@node0" "docker swarm join-token --quiet worker")
