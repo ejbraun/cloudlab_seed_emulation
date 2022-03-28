648aa3e32be27370ad9a9199275be7ac4160bf29
evan <evanbraun@berkeley.edu>
2022-03-27 21:22:46 -0600

    doing hack route

--- managerStartUp.sh
+++ managerStartUp.sh
@@ -22,6 +22,12 @@ filename='priority.txt'
 while read p; do
     sudo docker-compose -f "containers/${p}/docker-compose.yml" build
     sudo docker-compose -f "containers/${p}/docker-compose.yml" push
-    sudo docker stack deploy --compose-file "containers/$p/docker-compose.yml" "${p}"
+    docker service create \
+       --name "${p}-wrapper" \
+       --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
+       --mount type=bind,source="containers/${p}",target=/tmp/ \
+       --workdir /tmp/ \
+       docker/compose \
+       docker-compose up
     echo "Deployed $p to swarm"
 done < "$filename"
