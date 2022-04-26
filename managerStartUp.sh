23705920c7b9351ad13458b8f0be4fb590b1eea8
evan <evanbraun@berkeley.edu>
2022-04-25 22:12:41 -0600

    update startup scripts

--- managerStartUp.sh
+++ managerStartUp.sh
@@ -13,7 +13,7 @@ echo \
 sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/2.4.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 sudo docker swarm init --advertise-addr=$MANAGER_IP
